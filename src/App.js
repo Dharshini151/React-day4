61de683d3ac1c2c14655adc735ce0f8fd916b66a
Dharshini151 <727721euec024@skcet.ac.in>
2023-03-31 15:01:32 +0530

    msg

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+import Fruit from './component/product';
+import './App.css';
+
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <center>
+          <Fruit/>
+        </center>
+      </div>
+    )
+  }
+}
+export default App
